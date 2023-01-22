5920cf6423c7fc1d239077e5de9d1768857ebd4e
janvi01 <janvibajo1@gmail.com>
2023-01-22 12:04:07 +0530

    added background working and logo

--- background.js
+++ background.js
@@ -1,1 +1,30 @@
 //background.js
+chrome.alarms.onAlarm.addListener(() => {
+  chrome.notifications.create(
+    {
+      type: "basic",
+      iconUrl: "alert.jpeg",
+      title: "Take a Break",
+      message: "Have a sip of water or fix your posture gentleman!",
+    },
+    () => {}
+  );
+});
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(request);
+  if (request.time) {
+    createAlarm(request.time);
+    console.log(request.time);
+  }
+
+  sendResponse(() => {
+    return false;
+  });
+});
+
+function createAlarm(timeInt) {
+  chrome.alarms.create("take_break", {
+    delayInMinutes: Number(timeInt),
+    periodInMinutes: Number(timeInt),
+  });
+}
